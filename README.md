d48ba31f3d0de61b21887fed89e631a41ce195d0
Prithvi <prithvin19@gmail.com>
2024-03-06 22:32:39 +0530

    tweaks

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# test-button-muk-dev
-A test button component library made in tutorial
+This is a boilerplate to create an NPM package for one or more ReactJS custom hooks and components. I hope you find it helpful.
+
+## 🫶 Usage
+
+const IMG = [
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%201.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%202.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%203.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%204.jpg",
+    ratio: "aspect-box",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%205.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%202.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%205.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%204.jpg",
+    ratio: "aspect-box",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%205.jpg",
+    ratio: "aspect-square",
+  },
+  {
+    src: "https://ik.imagekit.io/webibee/Agency/img%201.jpg",
+    ratio: "aspect-square",
+  },
+];
+
+
+  ```
+<Masonry>
+      {IMG.map((item, idx) => (
+            <img
+              alt="..."
+              src={item.src}
+              className={` max-w-full rounded-md overflow-hidden object-cover object-center`}
+              key={idx}
+            />
+          ))}
+</Masonry>
+  ```
--- src/components/Masonry/Masonry.tsx
+++ src/components/Masonry/Masonry.tsx
@@ -1,47 +1,6 @@
 import React, { FC, ReactNode } from "react";
 import "./index.css";
-const IMG = [
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%201.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%202.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%203.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%204.jpg",
-    ratio: "aspect-box",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%205.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%202.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%205.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%204.jpg",
-    ratio: "aspect-box",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%205.jpg",
-    ratio: "aspect-square",
-  },
-  {
-    src: "https://ik.imagekit.io/webibee/Agency/img%201.jpg",
-    ratio: "aspect-square",
-  },
-];
+
 interface Props {
   children: ReactNode | ReactNode[];
 }
@@ -51,27 +10,13 @@ const Masonry: FC<Props> = ({ children }) => {
     <section className="w-full h-full p-10 mx-auto">
       <div className="space-y-4 overflow-hidden group">
         <div className="gap-3 px-6 space-y-4 columns-2 md:columns-3 rounded-xl scroll-smooth">
-          {IMG.map((item, idx) => (
-            <img
-              alt="..."
-              src={item.src}
-              className={` max-w-full rounded-md overflow-hidden object-cover object-center`}
-              key={idx}
-            />
-          ))}
+          {children}
         </div>
         <div
           aria-hidden="true"
           className="gap-3 px-6 space-y-4 columns-2 md:columns-3 rounded-xl scroll-smooth"
         >
-          {IMG.map((item, idx) => (
-            <img
-              alt="..."
-              src={item.src}
-              className={` max-w-full rounded-md overflow-hidden object-cover object-center`}
-              key={idx}
-            />
-          ))}
+          {children}
         </div>
       </div>
     </section>
